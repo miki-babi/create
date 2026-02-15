d943f178229af758a7252c2a98a4323cba42516a
miki-babi <mikiyasshiferaw@gmail.com>
2026-02-15 14:31:42 +0300

    fix init load

--- resources/views/miniapp-init.blade.php
+++ resources/views/miniapp-init.blade.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
     <title>Mini App Init</title>
+    <meta charset="UTF-8">
+<script src="https://telegram.org/js/telegram-web-app.js?59"></script>
 
-    <!-- Telegram Web App SDK -->
-    <script src="https://telegram.org/js/telegram-web-app.js"></script>
 </head>
 <body>
     <h1>Initializing Mini App...</h1>
 
     <script>
         const tg = window.Telegram?.WebApp;
+        if (!tg) alert("Not running inside Telegram Web App!");
 
-        if (!tg) {
-            console.error("Telegram WebApp object is not available");
-        } else {
-            fetch("{{ route('miniapp.init') }}", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRF-TOKEN": "{{ csrf_token() }}"
-                },
-                body: JSON.stringify({ initData: tg.initData })
-            })
-            .then(res => res.json())
-            .then(data => {
-                if (data.status === 'ok') {
-                    window.location.href = "{{ route('miniapp.main') }}";
-                } else {
-                    alert("Mini App init failed");
-                }
-            });
-        }
+        // Immediately send initData to server
+        fetch("{{ route('miniapp.init') }}", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": "{{ csrf_token() }}"
+            },
+            body: JSON.stringify({ initData: tg.initData })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.status === 'ok') {
+                // Redirect user to main Mini App view
+                window.location.href = "{{ route('miniapp.main') }}";
+            } else {
+                alert("Failed to initialize Mini App");
+            }
+        });
     </script>
 </body>
 </html>
